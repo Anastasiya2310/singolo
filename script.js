6fa5f71b30d146453258b15cdeac47c7f70da006
Anastasiya Kharitonova <kovalskaya.anastasiya@gmail.com>
2020-03-13 01:22:53 +0300

    feat: add toggle functionality for mobile screen

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
   const MENU = document.getElementById('menu');
   const MENU_ITEM = MENU.querySelectorAll('li');
-  const SLIDER = document.getElementById('slider');
+  const SLIDER_VERTICAL = document.getElementById('phone-vertical');
+  const SLIDER_HORIZONTAL = document.getElementById('phone-horizontal');
   
   MENU.addEventListener('click', event => {
-    MENU_ITEM.forEach(el => el.classList.remove('active'))
+    MENU_ITEM.forEach(el => el.classList.remove('active'));
     event.target.parentElement.classList.add('active');
   });
 
-  SLIDER.addEventListener('click', event => {
-  	//SLIDER.getElementsByClassName('phone').forEach(el => el.classList.remove('switch-off'));
-  	//event.target.classList.remove('switch-on');
+  SLIDER_VERTICAL.addEventListener('click', event => {
+  	event.target.classList.toggle('switch-off');
+  });
+
+  SLIDER_HORIZONTAL.addEventListener('click', event => {
+  	event.target.classList.toggle('switch-off');
   });
