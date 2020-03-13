b73778013291f875e37b5b27b912cb6836278d0f
Anastasiya Kharitonova <kovalskaya.anastasiya@gmail.com>
2020-03-14 00:57:16 +0300

    feat: add mixing functionality by click the tab

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+(function () {
+
   const MENU = document.getElementById('menu');
   const MENU_ITEM = MENU.querySelectorAll('li');
   const SLIDER = document.getElementById('slider');
+  const PORTFOLIO_LIST = document.getElementById('portfolio-list');
+  const TAGS = document.getElementById('tags');
   
   MENU.addEventListener('click', event => {
     MENU_ITEM.forEach(el => el.classList.remove('active'));
@@ -10,3 +14,24 @@
   SLIDER.addEventListener('click', event => {
   	event.target.closest('.phone').classList.toggle('switch-off');
   });
+
+  PORTFOLIO_LIST.addEventListener('click', event => {
+  	PORTFOLIO_LIST.querySelectorAll('li').forEach(el => el.classList.remove('active-work'));
+  	event.target.parentElement.classList.add('active-work');
+  });
+
+	TAGS.addEventListener('click', event => {	
+		let theFirstChild = PORTFOLIO_LIST.firstElementChild;
+		let theLastChild = PORTFOLIO_LIST.lastElementChild;
+		let portfolioItem = theFirstChild.nextElementSibling;
+		let lastElemSibling2 = theLastChild.previousElementSibling.previousElementSibling;
+		let lastElemSibling4 = theFirstChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling;
+
+		TAGS.querySelectorAll('li').forEach(el => el.classList.remove('active-tag'));
+		event.target.parentElement.classList.add('active-tag');
+		PORTFOLIO_LIST.insertBefore(portfolioItem, theFirstChild);
+		PORTFOLIO_LIST.insertBefore(theLastChild, lastElemSibling2);
+		PORTFOLIO_LIST.insertBefore(lastElemSibling4, lastElemSibling2);
+	});
+
+})();
